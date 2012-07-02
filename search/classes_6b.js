d77bc47276c98e6e4c5749e3d449be22531b62c3
Dominik Charousset <neverlord@mobi20.cpt.haw-hamburg.de>
2012-07-02 16:31:12 +0200

    version 0.2.1

--- search/classes_6b.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['keep_5fold_5ft',['keep_old_t',['../structcppa_1_1keep__old__t.html',1,'cppa']]]
-];
--- search/variables_63.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['compound_5fmember',['compound_member',['../group___type_system.html#ga3cae2f0daff2576a6266a82ea41b6dad',1,'cppa']]]
-];
